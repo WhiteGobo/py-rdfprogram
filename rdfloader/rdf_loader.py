34affcd99481364120a6167547a10dde302a0d5f
WhiteGobo <richardfechner@posteo.net>
2023-01-30 15:36:23 +0100

    changed debug-messages for load_from_graph wanted resources

--- rdfloader/rdf_loader.py
+++ rdfloader/rdf_loader.py
@@ -161,7 +161,11 @@ def load_from_graph( uri_to_constructor, rdf_graph, wanted_resources=None ):
     missing_wanted_resources = [x for x in starting_wanted_resources 
                                 if x not in asdf ]
     if missing_wanted_resources:
-        logger.warning(f"missing wanted resources: {missing_wanted_resources}")
+        logger.warning(f"missing wanted resources: %s"
+                       %([str(x) for x in missing_wanted_resources]))
+        logger.warning("further needed resources of the algorithm: %s"
+                       %([str(x) for x in wanted_resources if x not in asdf 
+                          and x not in missing_wanted_resources]) )
     return asdf
 
 def _get_creationinfo_to( target_resource, g: rdflib.Graph, \
