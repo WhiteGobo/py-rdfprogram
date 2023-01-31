d76870b34d95f99ba21c5fba4b872a6de0130709
WhiteGobo <richardfechner@posteo.net>
2023-01-31 17:31:03 +0100

    added files for testcase of evaluator

---
+++ programloader/test_src/myevaluator.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+import argparse
+
+def get_args():
+    Programdescription = "Add 1 and save to new file"
+    parser = argparse.ArgumentParser( description = Programdescription )
+    parser.add_argument( 'loadfile', type=str,
+                        help="filepath for loading data" )
+
+    program_args = parser.parse_args()
+    args = ( program_args.loadfile, )
+    return args
+
+def main(loadfile):
+    pass
+
+if __name__=="__main__":
+    args = get_args()
+    main( *args )
