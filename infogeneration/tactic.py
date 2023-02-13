a41b13f29ba41e027665046a46e0bdee93ca55df
WhiteGobo <richardfechner@posteo.net>
2023-02-13 18:15:16 +0100

    changed infogen.tactic.typecontrol

--- infogeneration/tactic.py
+++ infogeneration/tactic.py
@@ -4,14 +4,15 @@ import programloader
 
 class tactic:
     uses: programloader.program
-    def __init__(self, iri, uses: extc.info_attr_list(AUTGEN.uses)):
+    def __init__(self, uri, uses: extc.info_attr_list(AUTGEN.uses)):
+        self.uri = uri
         self._typecontrol_uses(uses)
         self.uses = uses
 
     @classmethod
-    def _typecontrol_uses(cls, uses):
-        myProgram.example_nodes
-        myProgram.generated_nodes
-        myProgram.old_axioms
-        myProgram.new_axioms
-
+    def _typecontrol_uses(cls, uses: list[programloader.program]):
+        for p in uses:
+            p.example_nodes
+            p.generated_nodes
+            p.old_axioms
+            p.new_axioms
--- infogeneration/test.py
+++ infogeneration/test.py
@@ -66,7 +66,6 @@ info_numbertoaxiom = f"""@prefix asdf: <http://example.com/> .
 """
 
 class TestInfogenerator( unittest.TestCase ):
-    @unittest.skip("asdf")
     def test_simple(self):
         g = rdflib.Graph().parse(format="ttl", data=f"""
             @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
