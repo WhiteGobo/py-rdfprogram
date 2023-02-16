fb8e62def8d8bbdb3e2bd4d47fab9d31c7652888
WhiteGobo <richardfechner@posteo.net>
2023-02-16 18:57:29 +0100

    removed unused programloader.evaluator.pythonprogram

--- programloader/class_evaluator.py
+++ programloader/class_evaluator.py
@@ -77,28 +77,3 @@ class evaluator(_iri_repr_class, useprogram.program):
         return new_axioms
 
 
-class python_program_container(_program):
-    """
-    """
-    def __init__(self, filepath):
-        assert os.path.exists( filepath )
-        self.filepath = filepath
-
-
-    def __call__(self, *args, **kwargs) -> str:
-        commandarray = ["python", str(self.filepath)]
-        for x in it.chain(args, it.chain.from_iterable(kwargs.items())):
-            try:
-                commandarray.append(x.as_inputstring())
-            except AttributeError:
-                commandarray.append(str(x))
-        q = subprocess.run(commandarray, capture_output=True)
-        try:
-            asdf = q.check_returncode()
-            program_return_str = q.stdout.decode()
-        except subprocess.CalledProcessError as err:
-            #print(f"failed to execute {self.filepath}", file=sys.stderr)
-            #print(q.stderr.decode(), file=sys.stderr)
-            raise ProgramFailed(q.stdout.decode(), q.stderr.decode()) from err
-        return program_return_str
-
