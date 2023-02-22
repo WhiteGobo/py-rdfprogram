c81d5ab5b05066e14515153f1db2a5e829185f88
WhiteGobo <richardfechner@posteo.net>
2023-02-22 13:30:23 +0100

    more info for infogeneration.test.test_simple

--- infogeneration/test.py
+++ infogeneration/test.py
@@ -33,14 +33,15 @@ info_adder_uri = f"""@prefix asdf: <http://example.com/> .
         @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
         @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
         @prefix testpath: <{mypath_prefix}> .
+        @prefix adder: <file://adder#> .
 
         <{adder_uri}> a proloa:program ;
             proloa:hasArgument _:add1, _:add2 .
-        _:add1 proloa:id 0 ;
+        adder:add1 proloa:id 0 ;
             rdfs:comment "loadfile" ;
             a proloa:arg ;
             proloa:describedBy _:addres1 .
-        _:add2 proloa:id 1 ;
+        adder:add2 proloa:id 1 ;
             rdfs:comment "savefile" ;
             a proloa:arg ;
             proloa:declaresInfoLike _:addres2 .
@@ -57,10 +58,11 @@ info_numbertoaxiom = f"""@prefix asdf: <http://example.com/> .
         @prefix proloa: <http://example.com/programloader/> .
         @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
         @prefix testpath: <{mypath_prefix}> .
+        @prefix info: <file://info#> .
 
         <{numbertoaxiom_uri}> a proloa:program ;
             proloa:hasArgument _:ntaArg .
-        _:ntaArg proloa:id 0 ;
+        info:ntaArg proloa:id 0 ;
             rdfs:comment "loadfile" ;
             a proloa:arg ;
             proloa:declaresInfoLike _:ntaNew ;
@@ -72,6 +74,70 @@ info_numbertoaxiom = f"""@prefix asdf: <http://example.com/> .
 """
 
 class TestInfogenerator( unittest.TestCase ):
+
+    def test_simple(self):
+        """
+        
+        :TODO: remake priority. It should be some kind of property bound
+            to the tactic.
+        """
+        g = rdflib.Graph().parse(format="ttl", data=f"""
+            @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+            @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+            @prefix autgen: <http://example.com/automaticgenerator#> .
+            @prefix testpath: <{mypath_prefix}> .
+            @prefix info: <file://info#> .
+            @prefix adder: <file://adder#> .
+
+            #automatic generation tactic
+            <file://mytactic> a autgen:tactic ;
+                autgen:uses <{adder_uri}>,
+                            <{numbertoaxiom_uri}> .
+                autgen:usesPriorityQueue _:adderQueue .
+            _:adderQueue autgen:controls <{adder_uri}> ;
+                rdfs:comment "prio = info:ntaArg->info:value" .
+            """)
+        g.parse(data = info_numbertoaxiom)
+        g.parse(data = info_adder_uri)
+        generated_objects = rl.load_from_graph(input_dictionary, g)
+        self.assertEqual(set(generated_objects), set(g.subjects()))
+
+        mytactic = generated_objects[rdflib.URIRef("file://mytactic")][0]
+
+        infograph = rdflib.Graph().parse(format="ttl", data=f"""
+            @prefix asdf: <http://example.com/> .
+            @prefix proloa: <http://example.com/programloader/> .
+            @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+            @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+
+            <{testnumber_uri}> a asdf:number ;
+                a proloa:link .
+            """)
+        asdf = mytactic.get_priorities(infograph)
+        app_resources = [s for s,p,o in asdf if p==RDF.a and o==PROLOA.app]
+        self.assertEqual(len(app_resources), 1, msg="There should be exactly "
+                                f"one BNode here: {app_resources}")
+        res_app = app_resources[0]
+        arg_resource = iter(g.query(f"""
+            SELECT ?x
+            WHERE {{
+                <{numbertoaxiom_uri}> proloa:hasArgument ?x .
+                ?x proloa:id 0 .
+            }}""")).__next__()[0]
+        expected_axioms = {
+                (res_app, RDF.a, PROLOA.app),
+                (res_app, PROLOA.executes, numbertoaxiom_uri),
+                (res_app, AUTGEN.priority, rdflib.Literal(0.0)),
+                (res_app, arg_resource, testnumber_uri),
+                (arg_resource, RDF.a, PROLOA.arg),
+                }
+        self.assertEqual(set(asdf), expected_axioms)
+
+        returnstring, new_axioms = mytactic.execute_first_app()
+        self.assertRaises(queue.Empty, mytactic.execute_first_app)
+        raise Exception(returnstring, new_axioms)
+
+
     def test_reasoning_support(self):
         g = rdflib.Graph().parse(format="ttl", data=f"""
             @prefix autgen: <http://example.com/automaticgenerator#> .
@@ -158,65 +224,9 @@ class TestInfogenerator( unittest.TestCase ):
                      Literal(3)),
                     ))
         self.assertEqual(set(newaxioms), set(shouldbeaxioms))
+        owl = URIRef("http://www.w3.org/2002/07/owl#").defrag()
+        newaxioms = [ax for ax in newaxioms if not any(owl in x for x in ax)]
 
-
-    @unittest.skip("asdf")
-    def test_simple(self):
-        """
-        
-        :TODO: remake priority. It should be some kind of property bound
-            to the tactic.
-        """
-        g = rdflib.Graph().parse(format="ttl", data=f"""
-            @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-            @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-            @prefix autgen: <http://example.com/automaticgenerator#> .
-            @prefix testpath: <{mypath_prefix}> .
-
-            #automatic generation tactic
-            <file://mytactic> a autgen:tactic ;
-                autgen:uses <{adder_uri}>,
-                            <{numbertoaxiom_uri}> .
-            """)
-        g.parse(data = info_numbertoaxiom)
-        g.parse(data = info_adder_uri)
-        generated_objects = rl.load_from_graph(input_dictionary, g)
-        self.assertEqual(set(generated_objects), set(g.subjects()))
-
-        mytactic = generated_objects[rdflib.URIRef("file://mytactic")][0]
-
-        infograph = rdflib.Graph().parse(format="ttl", data=f"""
-            @prefix asdf: <http://example.com/> .
-            @prefix proloa: <http://example.com/programloader/> .
-            @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-            @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-
-            <{testnumber_uri}> a asdf:number ;
-                a proloa:link .
-            """)
-        asdf = mytactic.get_priorities(infograph)
-        app_resources = [s for s,p,o in asdf if p==RDF.a and o==PROLOA.app]
-        self.assertEqual(len(app_resources), 1, msg="There should be exactly "
-                                f"one BNode here: {app_resources}")
-        res_app = app_resources[0]
-        arg_resource = iter(g.query(f"""
-            SELECT ?x
-            WHERE {{
-                <{numbertoaxiom_uri}> proloa:hasArgument ?x .
-                ?x proloa:id 0 .
-            }}""")).__next__()[0]
-        expected_axioms = {
-                (res_app, RDF.a, PROLOA.app),
-                (res_app, PROLOA.executes, numbertoaxiom_uri),
-                (res_app, AUTGEN.priority, rdflib.Literal(0.0)),
-                (res_app, arg_resource, testnumber_uri),
-                (arg_resource, RDF.a, PROLOA.arg),
-                }
-        self.assertEqual(set(asdf), expected_axioms)
-
-        returnstring, new_axioms = mytactic.execute_first_app()
-        self.assertRaises(queue.Empty, mytactic.execute_first_app)
-        raise Exception(returnstring, new_axioms)
         
 if __name__=="__main__":
     logging.basicConfig( level=logging.DEBUG )
