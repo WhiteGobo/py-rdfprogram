889488bb6430ee226c4434e83602f54259f1a6d9
WhiteGobo <richardfechner@posteo.net>
2023-03-03 19:44:07 +0100

    changed programloader.arg

--- programloader/__init__.py
+++ programloader/__init__.py
@@ -3,14 +3,15 @@
 """
 from . import PROLOA_NS
 
-from .useprogram import program, mutable_resource, arg, filelink, app
+from .useprogram import program, mutable_resource, filelink, app
+from .Arg import arg
 
 from . import useprogram
 
 input_dict = {\
         PROLOA_NS.program: useprogram.rdfprogram.from_rdf, \
         PROLOA_NS.mutable_resource: useprogram.mutable_resource,\
-        PROLOA_NS.arg: useprogram.arg,\
+        PROLOA_NS.arg: arg,\
         PROLOA_NS.link: useprogram.filelink,\
         PROLOA_NS.app: useprogram.app,\
         }
