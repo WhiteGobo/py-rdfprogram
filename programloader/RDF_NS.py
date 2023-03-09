a7dd328a4b8bfc69003be52f58a92e528cedee23
WhiteGobo <richardfechner@posteo.net>
2023-03-09 14:42:58 +0100

    added RDF.type

--- programloader/RDF_NS.py
+++ programloader/RDF_NS.py
@@ -3,4 +3,5 @@ from rdflib import URIRef
 
 _BASE = URIRef( "http://www.w3.org/1999/02/22-rdf-syntax-ns#" )
 a = URIRef( f"{_BASE}type" )
+type = URIRef( f"{_BASE}type" )
 subPropertyOf = URIRef( f"{_BASE}subPropertyOf" )
