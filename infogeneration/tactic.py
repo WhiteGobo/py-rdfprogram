f689c1b876ca693eff80e874db9df51e5066878d
WhiteGobo <richardfechner@posteo.net>
2023-03-15 13:23:21 +0100

    removed infogeneration.tactic.tactic's object containers

--- infogeneration/tactic.py
+++ infogeneration/tactic.py
@@ -243,29 +243,6 @@ class program_container:
 class tactic_priority_organizer(program_container):
     graphfinder: dict[myabc.program, rdfgraph_finder]
     """Mapping of used programs their graphfinders."""
-    #_app_prioritiyqueue: queue.PriorityQueue[float, myabc.app]
-    saved_objects: typ.Dict[rdflib.IdentifiedNode, myabc.program]
-    """All available programs, arguments and other objects. Used for 
-    loading additional objects from rdfgraphs.
-    """
-
-    def execute_first_app(self, rdfgraph: rdflib.Graph = None) \
-            -> (str, typ.Iterable[rdflib.graph._TripleType]):
-        """Returns the first app in the priorityQueue.
-        If a rdfgraph is given, checks in the graph the current priorities
-        and given apps. So if some other thing can execute the apps or
-        because of some other reason the graph changes, this will reload
-        all information.
-        :raises: queue.Empty
-        """
-        if rdfgraph is not None:
-            raise NotImplementedError("Yet no rdfgraph is supported as input")
-        try:
-            _, myApp = self._app_priorityqueue.get_nowait()
-        except queue.Empty:
-            raise
-        returnstring, new_axioms = myApp()
-        return returnstring, new_axioms
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -276,46 +253,6 @@ class tactic_priority_organizer(program_container):
             self.graphfinder[pro] = rdfgraph_finder(pro)
         self._app_priorityqueue = queue.PriorityQueue()
         self._current_used_rdfgraph = rdflib.Graph()
-        self.saved_objects = {}
-        for program in self.uses:
-            self.saved_objects[program.iri] = [program]
-            for arg in program.app_args:
-                self.saved_objects[arg.iri] = [arg]
-
-    def get_priorities(self, rdfgraph: rdflib.Graph) \
-            -> typ.Iterable[rdflib.graph._TripleType]:
-        """Generates info, which programs can be used on the given data
-        and what the priority of those programs are
-        """
-        pro: myabc.program
-        finder: rdfgraph_finder
-        arg_to_resource: dict[mybac.arg, rdflib.IdentifiedNode]
-        newaxioms: list[rdflib.graph._TripleType] = []
-        for pro, finder in self.graphfinder.items():
-            for arg_to_resource in finder._find_in_graph(rdfgraph):
-                app_identifier = rdflib.BNode()
-                tmp_axioms = finder.create_app(arg_to_resource, app_identifier)
-                newaxioms.extend(tmp_axioms)
-                tmp_prio = rdflib.Literal(self.calculate_priority())
-                newaxioms.append((app_identifier, AUTGEN.priority, tmp_prio))
-
-                tmp_graph = rdflib.Graph()
-                for ax in it.chain(tmp_axioms,rdfgraph):
-                    tmp_graph.add(ax)
-                self.saved_objects = rdfloader.load_from_graph(\
-                        programloader.input_dict, tmp_graph, \
-                        wanted_resources=[app_identifier], \
-                        iri_to_pythonobjects=self.saved_objects)
-                try:
-                    new_app = self.saved_objects[app_identifier][0]
-                except Exception as err:
-                    raise Exception(self.saved_objects) from err
-                self._app_priorityqueue.put((tmp_prio, new_app))
-
-        for ax in it.chain(newaxioms, rdfgraph):
-            self._current_used_rdfgraph.add(ax)
-
-        return newaxioms
 
     def calculate_priority(self):
         """Estimates the priority. This method isnt ready yet
